60b1703a34536a8550f88f6d2d2389b62560551e
mohammedsadiquepalakkal <sadique.pal@gmail.com>
2019-11-07 21:59:58 +0530

    refactor- code simplified by a function

--- todos/lib/todo_list_csvimporter.ex
+++ todos/lib/todo_list_csvimporter.ex
@@ -10,15 +10,30 @@ defmodule TodoList.CsvImporter do
     File.stream!(path)
     |> Stream.map(&String.replace(&1, "\n", ""))
     |> Stream.map(&String.split(&1, ","))
-    |> Enum.map(fn [k, v] -> {k, v} end)
-    |> Enum.map(fn {k, v} -> {String.split(k, "/"), v} end)
-    |> Enum.map(fn {[year, month, date], title} ->
-      {Date.from_erl(
-         {String.to_integer(year), String.to_integer(month), String.to_integer(date)}
-       ), title}
-    end)
-    |> Enum.map(fn {{:ok, k}, v} -> {k, v} end)
-    |> Enum.map(fn {k, v} -> %{date: k, title: v} end)
+    |> Enum.map(&to_enum(&1))
     |> TodoList.new()
+  end
+
+  # def to_enum(stream) do
+  #   stream
+  #   |> Enum.map(fn [k, v] -> {k, v} end)
+  #   |> Enum.map(fn {k, v} -> {String.split(k, "/"), v} end)
+  #   |> Enum.map(fn {[year, month, date], title} ->
+  #     {Date.from_erl!(
+  #        {String.to_integer(year), String.to_integer(month), String.to_integer(date)}
+  #      ), title}
+  #   end)
+  #   |> Enum.map(fn {k, v} -> %{date: k, title: v} end)
+  # end
+
+  def to_enum([k, v]) do
+    date =
+      k
+      |> String.split("/")
+      |> Enum.map(fn x -> String.to_integer(x) end)
+      |> List.to_tuple()
+      |> Date.from_erl!()
+
+    %{date: date, title: v}
   end
 end
